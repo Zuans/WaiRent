35009ddb7f47e5892620abc438b29c4175eb8915
Zuans <juandev.net@gmail.com>
2021-02-02 16:08:27 +0700

    edited waifu page

--- app/assets/js/waifu-all.js
+++ app/assets/js/waifu-all.js
@@ -3,10 +3,35 @@ const minPriceTxt = document.getElementById('waifu-min-price-txt');
 const maxPriceEl = document.getElementById('waifu-max-price');
 const maxPriceTxt = document.getElementById('waifu-max-price-txt');
 let minPriceVal = null;
+let maxPriceVal = null;
+
+// Switch Filter 
+const waifuWrapper = document.getElementById('waifus-wrapper');
+const filterSwitch = document.getElementById('filter-switch');
+const waifuFilterEl = document.getElementsByClassName('waifu-filter')[0];
+
+// Submit form
+const filterForm = document.getElementById('filter-form');
+filterForm.addEventListener('submit',function(e) {
+    e.preventDefault();
+    const waifuName = document.getElementById('waifu-name').value;
+    const waifuAgeMin = document.getElementById('waifu-min-age').value;
+    const waifuAgeMax = document.getElementById('waifu-max-age').value;
+    const hairType = document.getElementById('hair-type').value;
+    const dateTime = document.getElementById('date-time').value;
+    const test = {
+        waifuName,
+        waifuAgeMin,
+        waifuAgeMax,
+        hairType,
+        dateTime,
+        minPriceVal,
+        maxPriceVal
+    }
+    console.log(test);
+})
 
 // Change Price Value
-
-
 minPriceEl.addEventListener('change',function(e){
     const price = 1000 * this.value;
     const txt = `Rp.${price}`;
@@ -14,11 +39,21 @@ minPriceEl.addEventListener('change',function(e){
     minPriceVal = price;
 })
 
-console.log(maxPriceEl);
-
 maxPriceEl.addEventListener('change',function(e){
     const price = 1000 * this.value;
     const txt = `Rp.${price}`;
     maxPriceTxt.textContent = txt;
     maxPriceVal = price;
+})
+
+
+
+filterSwitch.addEventListener('click',function() {
+    if(waifuFilterEl.classList.contains('waifu-filter-active')) {
+        waifuWrapper.style.display = "flex";
+        waifuFilterEl.classList.remove('waifu-filter-active');
+        return;
+    }
+    waifuFilterEl.classList.add('waifu-filter-active');
+    waifuWrapper.style.display = 'block';
 })
