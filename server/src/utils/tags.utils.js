fc286bf69f8453e11d68c3fb18fd6c49fa924c91
Zuans <juandev.net@gmail.com>
2021-02-27 20:11:31 +0700

    add more property

--- server/src/utils/tags.utils.js
+++ server/src/utils/tags.utils.js
@@ -5,17 +5,34 @@ const hobbyModel = require('../model/hobby.model');
 const classTags = {
     "hair_type"  : {
         class : hairTypeModel,
+        tableName : "hair_types",
+        columnID : "hair_type_id",
+        columnVal : "name_type",
         method : 'findByNameOrID'
     },
     "date_time" : {
         class : dateTimeModel,
+        tableName : "date_times",
+        columnID : "date_time_id",
+        columnVal : "time",
         method : 'findByTimeOrID',
     },
     "hobby" : {
         class : hobbyModel,
+        tableName : "hobby",
+        columnID : "hobby_id",
+        method : "findByNameOrID",
+        columnVal : "name",
+        waifuQuery : "findByHobby"
+    },
+    "hobby_2" : {
+        class : hobbyModel,
+        tableName : "hobby",
+        columnID : "hobby_id",
+        columnVal : "name",
         method : "findByNameOrID",
         waifuQuery : "findByHobby"
-    }
+    },
 }
 
 const getClassTags = (classType) => {
